7e3d03072cccc823818cc31467ced5e18c1926c7
MiguelPaniagua <miguelpaniaguamuela@gmail.com>
2018-01-29 18:06:35 +0100

    >git

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ import config from './config'
 
 class Game extends Phaser.Game {
   constructor () {
-    const docElement = document.documentElementmaster
+    const docElement = document.documentElementmasterjeje
     /*const width = docElement.clientWidth < config.gameWidth ? config.gameWidth : docElement.clientWidth
     const height = docElement.clientHeight < config.gameHeight ? config.gameHeight : docElement.clientHeight*/
     const width = docElement.clientWidth
